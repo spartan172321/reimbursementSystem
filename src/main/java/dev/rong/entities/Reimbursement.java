7f7533718a75ab413b937a4599a82aedfe12f4de
spartan172321 <chronger1115@gmail.com>
2020-04-10 18:49:41 -0400

    updated the entities

--- src/main/java/dev/rong/entities/Reimbursement.java
+++ src/main/java/dev/rong/entities/Reimbursement.java
@@ -1,7 +1,65 @@
 package dev.rong.entities;
 
 public class Reimbursement {
-	private int rId;
+	private int rid;
 	private double amount;
 	private String description;
+	private int eid;
+	private int mid;
+	
+	public Reimbursement(double amount, String description, int eid) {
+		super();
+		this.amount = amount;
+		this.description = description;
+		this.eid = eid;
+	}
+
+	public int getRid() {
+		return rid;
+	}
+
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public int getMid() {
+		return mid;
+	}
+
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+
+	@Override
+	public String toString() {
+		return "Reimbursement [rid=" + rid + ", amount=" + amount + ", description=" + description + ", eid=" + eid
+				+ ", mid=" + mid + "]";
+	}
+	
+	
+	
 }
