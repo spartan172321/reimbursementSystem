58d8574da221c91d7c5cd619deadd3aaf7195cca
spartan172321 <chronger1115@gmail.com>
2020-04-13 15:44:50 -0400

    Created Service Interfaces

---
+++ src/main/java/dev/rong/services/EmployeeServices.java
@@ -0,0 +1,14 @@
+package dev.rong.services;
+
+import java.util.List;
+
+import dev.rong.entities.Reimbursement;
+
+public interface EmployeeServices {
+	
+	boolean employeeLogin(String username, String password);
+	Reimbursement createReimbursement(Reimbursement r);
+	Reimbursement updateReimbursement(Reimbursement r);
+	List<Reimbursement> viewAllEmployeeReimbursements(int eid);
+	boolean deleteReimbursement(Reimbursement r);
+}
