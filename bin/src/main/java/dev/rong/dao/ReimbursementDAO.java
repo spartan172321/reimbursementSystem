cfc7143e6d6f6c41bf3abe03a00c266fce50ab38
spartan172321 <chronger1115@gmail.com>
2020-04-15 14:55:25 -0400

    added services

---
+++ bin/src/main/java/dev/rong/dao/ReimbursementDAO.java
@@ -0,0 +1,23 @@
+package dev.rong.dao;
+
+import java.util.List;
+
+import dev.rong.entities.Reimbursement;
+
+public interface ReimbursementDAO {
+	
+	Reimbursement createReimbursement(Reimbursement r);
+	
+	Reimbursement getReimbursementByRid(int rid);
+	
+	List<Reimbursement> getReimbursementsByEid(int eid);
+	
+	List<Reimbursement> getReimbursementsByMid(int mid);
+	
+	List<Reimbursement> getAllReimbursements();
+	
+	Reimbursement updateReimbursementEntry(Reimbursement r);
+	
+	Reimbursement updateReimbursementStatus(Reimbursement r);
+
+}
